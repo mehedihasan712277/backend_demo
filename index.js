400f8b89c30be3f0fcb968769499eb0f873e255d
mehedihasan712277 <mehedihasan712277@gmail.com>
2025-04-04 03:13:17 +0600

    basic middleware setup done

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./src/db/db.js";
+import app from "./src/app.js";
 
 dotenv.config({
     path: ".env"
 })
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed !!!", error);
+    })
 
 // (async () => {
 //     try {
