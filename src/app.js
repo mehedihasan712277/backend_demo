b05714e003aaa0423f6f28851401a8f5d5d0e172
mehedihasan712277 <mehedihasan712277@gmail.com>
2025-04-05 08:41:53 +0600

    controller and router testing done

--- src/app.js
+++ src/app.js
@@ -9,9 +9,16 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express, json({ limit: "16kb" }))
+app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//import router
+import userRouter from "./routes/user.route.js"
+
+
+//decalre router
+app.use("/v1/user", userRouter);
 
 export default app;
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import asyncHandler from "../utils/asynchandler.js";
+
+// const registerUser = asyncHandler(async (req, res) => {
+//     res.status(200).json({
+//         message: "ok"
+//     })
+// })
+
+
+const registerUser = async (req, res) => {
+    res.status(200).json({
+        message: "ok"
+    })
+}
+
+export { registerUser }
