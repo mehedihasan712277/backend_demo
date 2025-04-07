90f5e4c321d72e12a25fb5ebfde711f1c96d86c4
mehedihasan712277 <mehedihasan712277@gmail.com>
2025-04-07 23:16:21 +0600

    empty field validation done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import asyncHandler from "../utils/asynchandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    const { fullName, email, username, password } = req.body
+    //console.log("email: ", email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
 })
 
 
